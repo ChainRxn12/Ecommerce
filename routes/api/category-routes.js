da4af704807c4a9a975bdfcb196adcb31f18f2e4
Billy Dorman <b.d.dorman12@gmail.com>
2021-07-26 10:57:11 -0500

    create delete category by id, category-routes.js api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -89,6 +89,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).JSON({ message: 'Category Was Not Found' });
+      return;
+    }
+    res.JSON(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).JSON(err);
+  });
 });
 
 module.exports = router;
