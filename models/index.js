938f8ed5b8bb02e4a4ba6dfac9d13d159498ecd2
Billy Dorman <b.d.dorman12@gmail.com>
2021-07-28 10:46:54 -0500

    define columns tag.js models/ work on index.js in models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.belongsTo(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  through: ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
